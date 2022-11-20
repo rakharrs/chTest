4813502f668510ae9d35647b1e1d34c5ec5a251c
rakhars <yoann.r0dyox@gmail.com>
2022-11-20 21:57:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import chessengine.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Window wind = Window.createWindow();
+    }
+}
